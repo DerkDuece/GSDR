760c66890557321c72b1ad842ecd3508eca0b103
Tich none <tichman27@gmail.com>
2023-04-22 15:20:55 +0200

    Update config.lua

--- resources/[addedmods]/[renzu]/vImageCreator/config.lua
+++ resources/[addedmods]/[renzu]/vImageCreator/config.lua
@@ -2,7 +2,7 @@
 Config = {}
 Config.DiscordWebHook = 'https://discord.com/api/webhooks/905382029066113044/m4O7_m3jwcyzbL0WnwmujG0V79lwQI-3tl_iWRviXbbKo1RKoXs2-vtPDGpKdC7tHkpS'
 Config.save = 'json' -- kvp, json
-Config.vehicle_table = 'vehicles' -- vehicle table must have model column (name not hash)
+Config.vehicle_table = 'player_vehicles' -- vehicle table must have model column (name not hash)
 Config.useSQLvehicle = true -- use mysql async to fetch vehicle table else SqlVehicleTable will use
 Config.SqlVehicleTable = QBCore and QBCore.Shared and QBCore.Shared.Vehicles and QBCore.Shared.Vehicles or {} -- example qbcore shared vehicle
 
