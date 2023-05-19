f3e374638b9c36baf7078cb9b962455ac0eb219a
Tich none <tichman27@gmail.com>
2023-05-19 12:11:01 +0200

    updating exports to pefcl

--- resources/[addedmods]/[civilianjobs]/cad-postalop/server.lua
+++ resources/[addedmods]/[civilianjobs]/cad-postalop/server.lua
@@ -6,13 +6,13 @@ RegisterNetEvent('cad-delivery:cash', function(currentJobPay, value)
 	local _source = source
 	local Player = QBCore.Functions.GetPlayer(_source)
 	if value == "job" then
-		Player.Functions.AddMoney("bank", currentJobPay, "postal-job")
+		exports.pefcl:addBankBalance(source, currentJobPay)
 		TriggerClientEvent("QBCore:Notify", _source, "You recieved payslip of $" .. currentJobPay)
 	elseif value == "add" then
-		Player.Functions.AddMoney("cash", currentJobPay, "postal-deposit-return")
+		exports.pefcl:addCash(source, currentJobPay)
 		TriggerClientEvent("QBCore:Notify", _source, "Your $" .. currentJobPay .. " deposit was returned.")
 	elseif value == "remove" and Player.PlayerData.money.cash >= currentJobPay then
-		Player.Functions.RemoveMoney("cash", currentJobPay, "postal-deposit-pay")
+		exports.pefcl:removeCash(source, currentJobPay)
 		TriggerClientEvent("QBCore:Notify", _source, "Your $" .. currentJobPay .. " was taken as deposit.")
 	end
 end)
