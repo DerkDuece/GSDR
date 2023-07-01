47030c4ac5ab75d0cd36629d26cf7d8973872b74
Tich none <tichman27@gmail.com>
2023-07-01 09:23:45 +0200

    Fourth Amendment

---
+++ resources/[addedmods]/[police]/ars_policejob/client/bridge/esx.lua
@@ -0,0 +1,102 @@
+local ESX = GetResourceState('es_extended'):find('start') and exports['es_extended']:getSharedObject() or nil
+
+if not ESX then return end
+
+function toggleClothes(toggle, clothes)
+    if toggle then
+        utils.debug("Putting on clothes")
+
+        local playerData = ESX.GetPlayerData()
+        local playerPed = cache.ped
+        local jobGrade = playerData.job.grade
+
+        utils.debug("Job Grade " .. jobGrade)
+
+        if Config.ClothingScript and Config.ClothingScript ~= 'core' then
+            local model = exports['illenium-appearance']:getPedModel(playerPed)
+
+            if model == 'mp_m_freemode_01' then
+                data = clothes.male[jobGrade] or clothes.male[1]
+            elseif model == 'mp_f_freemode_01' then
+                data = clothes.female[jobGrade] or clothes.female[1]
+            end
+
+            utils.debug("Using " .. Config.ClothingScript)
+
+            exports[Config.ClothingScript]:setPedComponents(playerPed, {
+                { component_id = 3, texture = 0, drawable = data['arms'] },
+                {
+                    component_id = 8,
+                    texture = data['tshirt_2'],
+                    drawable = data['tshirt_1']
+                },
+                {
+                    component_id = 11,
+                    texture = data['torso_2'],
+                    drawable = data['torso_1']
+                },
+                {
+                    component_id = 9,
+                    texture = data['bproof_2'],
+                    drawable = data['bproof_1']
+                },
+                {
+                    component_id = 10,
+                    texture = data['decals_2'],
+                    drawable = data['decals_1']
+                },
+                {
+                    component_id = 7,
+                    texture = data['chain_2'],
+                    drawable = data['chain_1']
+                },
+                {
+                    component_id = 4,
+                    texture = data['pants_2'],
+                    drawable = data['pants_1']
+                },
+                {
+                    component_id = 6,
+                    texture = data['shoes_2'],
+                    drawable = data['shoes_1']
+                },
+                {
+                    component_id = 5,
+                    texture = data['bag_color'],
+                    drawable = data['bag']
+                },
+            })
+        elseif Config.ClothingScript == 'core' then
+            utils.debug("Using " .. Config.ClothingScript)
+            ESX.TriggerServerCallback('esx_skin:getPlayerSkin', function(skin, jobSkin)
+                local gender = skin.sex
+
+                if gender == 1 then
+                    TriggerEvent('skinchanger:loadClothes', skin, clothes.male[jobGrade] or clothes.male[1])
+                else
+                    TriggerEvent('skinchanger:loadClothes', skin, clothes.female[jobGrade] or clothes.female[1])
+                end
+            end)
+        end
+    else
+        utils.debug("Putting civil clothes")
+
+        ESX.TriggerServerCallback('esx_skin:getPlayerSkin', function(skin, jobSkin)
+            TriggerEvent('skinchanger:loadSkin', skin)
+            TriggerEvent('esx:restoreLoadout')
+        end)
+    end
+end
+
+function getPlayerJobGrade()
+    local playerData = ESX.GetPlayerData()
+    local jobGrade = playerData.job.grade
+
+    return jobGrade
+end
+
+function isPoliceOfficer()
+    local playerData = ESX.GetPlayerData()
+
+    return playerData.job.name == Config.PoliceJobName
+end
---
+++ resources/[addedmods]/[police]/ars_policejob/client/functions/utils.lua
@@ -0,0 +1,43 @@
+local CreatePed = CreatePed
+local FreezeEntityPosition = FreezeEntityPosition
+local SetEntityInvincible = SetEntityInvincible
+local SetBlockingOfNonTemporaryEvents = SetBlockingOfNonTemporaryEvents
+local SetModelAsNoLongerNeeded = SetModelAsNoLongerNeeded
+local RegisterNetEvent = RegisterNetEvent
+
+
+utils = {}
+peds = {}
+
+function utils.createPed(name, coords)
+    local model = lib.requestModel(name)
+
+    if not model then return end
+
+    local ped = CreatePed(0, model, coords, false, false)
+
+    FreezeEntityPosition(ped, true)
+    SetEntityInvincible(ped, true)
+    SetBlockingOfNonTemporaryEvents(ped, true)
+    SetModelAsNoLongerNeeded(model)
+
+    table.insert(peds, ped)
+
+    return ped
+end
+
+function utils.showNotification(msg, type)
+    lib.notify({
+        title = 'Ars Policejob',
+        description = msg,
+        type = type and type or 'info'
+    })
+end
+
+function utils.debug(msg)
+    if Config.Debug then
+        print(("^3DEBUG: %s ^7"):format(msg))
+    end
+end
+
+RegisterNetEvent('ars_policejob:showNotification', utils.showNotification)
