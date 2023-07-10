c993ecfbcf0287be2ffd3decba9dd2314cbabe77
Tich none <tichman27@gmail.com>
2023-07-10 18:28:30 +0200

    several updates

--- resources/[addedmods]/[mz]/mz-scrap/config.lua
+++ resources/[addedmods]/[mz]/mz-scrap/config.lua
@@ -10,6 +10,8 @@ Config.NotifyType = 'qb'                  -- notification type: 'qb' for qb-core
 
 Config.mzskills = true                      -- change to "false" if you wish to use this resource without using mz-skills
 
+Config.ScrapXPSkill = "Scraping"            -- Take the name of your scrap skill from mz-skills (make sure they are exactly the same)
+
 Config.SellLocation = {
     [1] = {
             coords = vector3(-54.9, 6392.3, 31.62),
@@ -281,38 +283,5 @@ Config.CarItems = {
     [11] = {
         item = "sparkplugs",
         price = math.random(270, 310)
-    },
-    [12] = {
-        item = "car_hood",
-        price = math.random(300, 400)
-    },
-    [13] = {
-        item = "car_tire",
-        price = math.random(50, 100)
-    },
-    [14] = {
-        item = "car_door",
-        price = math.random(150, 200)
-    },
-    [15] = {
-        item = "car_trunk",
-        price = math.random(200, 300)
-    },
-    [16] = {
-        item = "carpart_wheel",
-        price = math.random(50, 75)
-    },
-    [17] = {
-        item = "carpart_hood",
-        price = math.random(200, 275)
-    },
-    [18] = {
-        item = "carpart_door",
-        price = math.random(200, 275)
-    },
-    [19] = {
-        item = "carpart_trunk",
-        price = math.random(200, 275)
     }
-
 }
