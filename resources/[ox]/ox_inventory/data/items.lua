d2c5a6d1f42e8aef3ba726063adf74343d1c4d99
Tich none <tichman27@gmail.com>
2023-07-10 14:30:37 +0200

    updating inventory

--- resources/[ox]/ox_inventory/data/items.lua
+++ resources/[ox]/ox_inventory/data/items.lua
@@ -12538,5 +12538,34 @@ return {
 		label = 'Steel File',
 		weight = 300,
 	},
-
+	['blank_prescription'] = {
+		label = 'Blank Prescription',
+		description = nil,
+		weight = 10,
+		stack = true,
+		close = true
+	},
+	
+	['signed_prescription'] = {
+		label = 'Prescription',
+		description = nil,
+		weight = 10,
+		stack = true,
+		close = true
+	},
+	
+	['oxy_bottle'] = {
+		label = 'Oxy Bottle',
+		description = 'A bottle of addiction',
+		weight = 115,
+		stack = true,
+		close = true
+	},
+	
+	['oxycontin'] = {
+		label = 'Oxycontin',
+		weight = 5,
+		stack = true,
+		close = true
+	},
 }
